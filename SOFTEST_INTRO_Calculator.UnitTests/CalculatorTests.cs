5d949f4518e54617285bbf2bc79ee6758a476266
YWY2002 <wenyuanyik@gmail.com>
2026-09-20 11:59:22 +0800

    Lab 1

---
+++ SOFTEST_INTRO_Calculator.UnitTests/CalculatorTests.cs
@@ -0,0 +1,163 @@
+using SOFTEST_INTRO_Calculator;
+using NUnit.Framework;
+using System;
+
+namespace SOFTEST_INTRO_Calculator.UnitTests;
+
+public class CalculatorTests
+
+{
+
+    private Calculator _calculator = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _calculator = new Calculator();
+    }
+
+    [Test]
+    public void Add_TwoPositiveNumbers_ReturnsSum()
+    {
+        // Arrange: the calculator is created in SetUp.
+        // Act
+        double result = _calculator.Add(10, 20);
+        // Assert
+        Assert.That(result, Is.EqualTo(30));
+    }
+
+    [Test]
+    [TestCase(0, 0, 0)]
+    [TestCase(0, 5, 5)]
+    [TestCase(-3, 8, 5)]
+    [TestCase(0.1, 0.2, 0.3)]
+    public void Add_RepresentativeInputs_ReturnsSum(
+    double a, double b, double expected)
+    {
+        double result = _calculator.Add(a, b);
+        Assert.That(result, Is.EqualTo(expected).Within(1e-9));
+    }
+
+    [TestCase(double.MaxValue, double.MaxValue)]
+    [TestCase(double.MinValue, double.MinValue)]
+    public void Add_DoubleOverflow_ThrowsOverflowException(double a, double b)
+    {
+        Assert.That(() => _calculator.Add(a, b), Throws.TypeOf<OverflowException>());
+    }
+
+    [TestCase(0, 0, 0)]
+    [TestCase(3, 8, -5)]
+    [TestCase(-10, 8, -18)]
+    [TestCase(-4.5, -3.5, -1)]
+    public void Subtract_RepresentativeInputs_ReturnSubtraction(double a, double b, double expected)
+    {
+        double result = _calculator.Subtract(a, b);
+        Assert.That(result, Is.EqualTo(expected).Within(1e-9));
+    }
+
+    [TestCase(double.MinValue, double.MaxValue)]
+    public void Subtract_DoubleOverflow_ThrowsOverflowException(double a, double b)
+    {
+        Assert.That(() => _calculator.Subtract(a, b), Throws.TypeOf<OverflowException>());
+    }
+
+    [TestCase(double.MinValue, double.MaxValue)]
+    [TestCase(double.MaxValue, double.MaxValue)]
+    [TestCase(double.MinValue, double.MinValue)]
+    public void Multiply_DoubleOverflow_ThrowsOverflowException(double a, double b)
+    {
+        Assert.That(() => _calculator.Multiply(a, b), Throws.TypeOf<OverflowException>());
+    }
+
+    [TestCase(15, 0)]
+    [TestCase(0, 0)]
+    public void Divide_ZeroDivisor_ThrowsArgumentException(double a, double b)
+    {
+        Assert.That(() => _calculator.Divide(a, b),
+        Throws.TypeOf<ArgumentException>());
+    }
+
+    [Test]
+    public void Factorial_InputZero_ReturnOne()
+    {
+        long result = _calculator.Factorial(0);
+        Assert.That(result, Is.EqualTo(1L));
+    }
+
+    [TestCase(-1)]
+    [TestCase(21)]
+    public void Factorial_NegativeInput_ThrowsArgumentOutOfRangeException(int n)
+    {
+        Assert.That(() => _calculator.Factorial(n), Throws.TypeOf<ArgumentOutOfRangeException>());
+    }
+
+    [TestCase(1, 1)]
+    [TestCase(5, 120)]
+    [TestCase(20, 2432902008176640000)]
+    public void Factorial_Inputs_ReturnProduct(int n, long expected)
+    {
+        Assert.That(_calculator.Factorial(n), Is.EqualTo(expected));
+    }
+
+    [TestCase(4, 0)]
+    [TestCase(0, 7)]
+    [TestCase(0, 0)]
+    [TestCase(-9, 10)]
+    [TestCase(6, -11)]
+    [TestCase(-5, -6)]
+    [TestCase(-8, 0)]
+    [TestCase(0, -7)]
+    public void TriangeArea_ZeroNegativeInput_ThrowsArgumentOutOfRangeException(double a, double b)
+    {
+        Assert.That(() => _calculator.TriangleArea(a, b), Throws.TypeOf<ArgumentOutOfRangeException>());
+
+    }
+
+    [TestCase(3, 4, 6)]
+    public void TriangleArea_Inputs_ReturnArea(double a, double b, double expected)
+    {
+        Assert.That(_calculator.TriangleArea(a, b), Is.EqualTo(expected).Within(1e-6));
+    }
+
+    [TestCase(1, Math.PI)]
+    public void CircleArea_Inputs_ReturnArea(double radius, double expected)
+    {
+        Assert.That(_calculator.CircleArea(radius), Is.EqualTo(expected).Within(1e-6));
+    }
+
+    [TestCase(0)]
+    [TestCase(-20)]
+    public void CircleArea_Zero_ThrowsArgumentOutOfRangeException(double a)
+    {
+        Assert.That(() => _calculator.CircleArea(a), Throws.TypeOf<ArgumentOutOfRangeException>());
+    }
+
+    // [TestCase(-4, 5)]
+    // [TestCase(4, 5)]
+    // [TestCase(6, 21)]
+    // public void UnknowFunction_ValueConstriant_ThrowsArgumentOutOfRangeException(int n, int r)
+    // {
+    //     Assert.That(() => _calculator.UnknownFunctionA(n, r), Throws.TypeOf<ArgumentOutOfRangeException>());
+    //     Assert.That(() => _calculator.UnknownFunctionB(n, r), Throws.TypeOf<ArgumentOutOfRangeException>());
+    // }
+    
+    // [TestCase(5, 5, 120)]
+    // [TestCase(5, 4, 120)]
+    // [TestCase(5, 3, 60)]
+    // [TestCase(5, 0, 1)]
+    // [TestCase(0, 0, 1)]
+    // public void UnknowFunctionA_Inputs_ReturnValue(int n, int r, double expected)
+    // {
+    //     Assert.That(_calculator.UnknownFunctionA(n, r), Is.EqualTo(expected).Within(1e-6));
+    // }
+
+    // [TestCase(5, 5, 1)]
+    // [TestCase(5, 4, 5)]
+    // [TestCase(5, 3, 10)]
+    // [TestCase(5, 0, 1)]
+    // [TestCase(0, 0, 1)]
+    // public void UnknowFunctionB_Inputs_ReturnValue(int n, int r, double expected)
+    // {
+    //     Assert.That(_calculator.UnknownFunctionB(n, r), Is.EqualTo(expected).Within(1e-6));
+    // }
+}
---
+++ SOFTEST_INTRO_Calculator/Calculator.cs
@@ -0,0 +1,132 @@
+namespace SOFTEST_INTRO_Calculator;
+using System;
+
+public class Calculator
+
+{
+    public double Add(double a, double b)
+    {
+        double res = a + b;
+        if (double.IsInfinity(res))
+        {
+            throw new OverflowException();
+        }
+        return res;
+
+    }
+    public double Subtract(double a, double b)
+    {
+        double res = a - b;
+        if (double.IsInfinity(res))
+        {
+            throw new OverflowException();
+        }
+        return res;
+    }
+    public double Multiply(double a, double b)
+    {
+        double res = a * b;
+        if (double.IsInfinity(res))
+        {
+            throw new OverflowException();
+        }
+        return res;
+    }
+
+    // Starter version: complete the zero-divisor rule in section 5.
+    public double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            throw new ArgumentException();
+        }
+        else
+        {
+            return a / b;
+        }
+    }
+
+    public long Factorial(int n)
+    {
+        if (n < 0 || n > 20)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        if (n == 0)
+        {
+            return 1;
+        }
+        long res = n * Factorial(n - 1);
+        return res;
+    }
+
+    public double TriangleArea(double height, double width)
+    {
+        if (height <= 0 || width <= 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        double area = (height * width) / 2;
+        return area;
+    }
+
+    public double CircleArea(double radius)
+    {
+        if (radius <= 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        double pi = Math.PI;
+        return pi * radius * radius;
+    }
+
+    public double UnknownFunctionA(int n, int r)
+    {
+        if (n < 0 || n > 20 || n < r)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        if (r < 0 || r > 20)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        // 2 factorial one divide 
+        return Factorial(n) / Factorial(n - r);
+        
+    }
+    public double UnknownFunctionB(int n, int r)
+    {
+        if (n < 0 || n > 20 || n < r)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        if (r < 0 || r > 20)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        // 3 factorial, one multiplication, one div
+        return Factorial(n) / Factorial(n - r);
+    }
+
+    public double DoOperation(double a, double b, string op)
+    {
+
+        return op switch
+        {
+
+            "a" => Add(a, b),
+
+            "s" => Subtract(a, b),
+
+            "m" => Multiply(a, b),
+
+            "d" => Divide(a, b),
+
+            _ => throw new ArgumentException("Unknown operation.")
+
+        };
+
+    }
+
+}
